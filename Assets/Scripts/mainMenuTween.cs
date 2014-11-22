e4f462e5e5e5ccebcf62890941f015ab40a01892
juste119 <mike@playnimbus.com>
2014-11-22 10:40:41 -0800

    endgame stuff

--- Assets/Scripts/mainMenuTween.cs
+++ Assets/Scripts/mainMenuTween.cs
@@ -3,16 +3,27 @@ using System.Collections;
 
 public class mainMenuTween : MonoBehaviour {
 
+    public bool direction;
+
     Hashtable ht = new Hashtable();
 
 	// Use this for initialization
 	void Start () {
 
-        ht.Add("z",1.75f);
-        ht.Add("time",0.5f);
-        ht.Add("easetype", iTween.EaseType.linear);
-        ht.Add("loopType", iTween.LoopType.pingPong);
-
+        if (direction == true)
+        {
+            ht.Add("z", 1.75f);
+            ht.Add("time", 0.5f);
+            ht.Add("easetype", iTween.EaseType.linear);
+            ht.Add("loopType", iTween.LoopType.pingPong);
+        }
+        else
+        {
+            ht.Add("z", -1.75f);
+            ht.Add("time", 0.6f);
+            ht.Add("easetype", iTween.EaseType.linear);
+            ht.Add("loopType", iTween.LoopType.pingPong);
+        }
         iTween.RotateTo(gameObject, ht);
 	
 	}
---
+++ Assets/endGameTrigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endGameTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            StartCoroutine(BeginCutScene());
+        }
+
+    }
+
+
+    IEnumerator BeginCutScene()
+    {       
+         yield return 6.8f;    //Wait one frame
+         gameObject.GetComponent<AudioSource>().Play();
+  
+    }
+}
