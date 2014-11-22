1ca3123b40cd13a9b0c65d278044264f0c5d3e0d
Nick Mudry <nick@mudry.me>
2014-11-22 03:22:25 -0500

    Tiling Works

---
+++ Assets/Scripts/BackgroundSpawnScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundSpawnScript : MonoBehaviour {
+    GameObject player;
+    public GameObject background;
+    float timer;
+    public float timerReset = 2.5f;
+    public float distanceFromCameraCenter = 18f;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player.GetComponent<ShoeController>().gameState == ShoeController.GameStates.play)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timerReset)
+            {
+                GameObject newBackgrond = (GameObject)Instantiate(background, new Vector3(Camera.main.transform.position.x + distanceFromCameraCenter, 0.6000061f, 2.42f), new Quaternion(0f, 0f, 0f, 0f));
+                timer = 0f;
+            }
+        }
+	}
+}
