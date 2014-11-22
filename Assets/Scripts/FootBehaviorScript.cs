b8845189a7cb3a63c02652f65b178e9cdadafa79
juste119 <mike@playnimbus.com>
2014-11-22 06:08:01 -0800

    footshadow fixed, and ui pushed to top left

--- Assets/Scripts/FootBehaviorScript.cs
+++ Assets/Scripts/FootBehaviorScript.cs
@@ -2,50 +2,22 @@
 using System.Collections;
 
 public class FootBehaviorScript : MonoBehaviour {
-    GameObject shoe;
-    bool dropTheShoe;
-    public float dropSpeed;
+
     public GameObject shadow;
 	// Use this for initialization
 	void Start () {
-     //   shoe = GameObject.Find("Shoe");
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	    if (dropTheShoe)
-        {
-       //     Drop();
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-        //    ToggleDrop();
-        }
-	}
-    
-    public void ToggleDrop()
+    void Update()
     {
-    //    dropTheShoe = !dropTheShoe;
     }
-
-    void SnapToShoe()
-    {
-   //     gameObject.transform.SetParent(shoe.transform);
-   //     gameObject.transform.localPosition = new Vector3(0f, shoe.transform.position.y + 7.5f, 0f);
-    }
-
-    public void Drop()
-    {
-     //   gameObject.transform.SetParent(null,true);
-     //   gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - dropSpeed * Time.deltaTime, gameObject.transform.position.x);
-    }
-
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "needle")
         {
-            shadow.rigidbody.velocity = new Vector3(shadow.rigidbody.velocity.x * -1, shadow.rigidbody.velocity.y * -1, shadow.rigidbody.velocity.z * -1);
+            gameObject.rigidbody.velocity = new Vector3(gameObject.rigidbody.velocity.x * -1, gameObject.rigidbody.velocity.y * -1, gameObject.rigidbody.velocity.z * -1);
         }
     }
 }
--- Assets/Scripts/ShadowBehaviorScript.cs
+++ Assets/Scripts/ShadowBehaviorScript.cs
@@ -2,105 +2,24 @@
 using System.Collections;
 
 public class ShadowBehaviorScript : MonoBehaviour {
-    public GameObject shoe;
-    GameObject shadow; //TODO: Replace all the shadow 
-    GameObject foot;
 
-    public float shadowSeekSpeed;
-    public float stompTime; //Time that the foot will graciously insert itself into the shoe. 
-    public float wiggleRoom;
-    float shadowTimer;
+    public GameObject foot;
 
-    float spawnXPosition;
-
-    bool isSeeking;
-    bool playerFound;
-    bool hitRightBound;
 	// Use this for initialization
 	void Start () {
-        shadow = gameObject; //I derped. This will be changed / removed soon. Originally was a footscript and this was not changed because lazy. 
-        foot = GameObject.Find("Foot");
-        isSeeking = true;
-        hitRightBound = false;
-        spawnXPosition = gameObject.transform.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (isSeeking)
-        {
-   //         shadow.gameObject.active = true;
-   //         ShadowSeek();
-        }
-        else if (!isSeeking)
-        {
-            //Hide the shadow and do not seek and whatnot stuffs.
-      //      shadow.gameObject.active = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            DropFoot();
-        }
 
 	}
 
-    //This will trigger the shadow to start seeking the shoe. 
-    void ShadowSeek()
-    {
-        if (hitRightBound)
-        {
-            shadow.gameObject.rigidbody.MovePosition(new Vector3(shadow.gameObject.transform.position.x - shadowSeekSpeed * Time.deltaTime, shadow.gameObject.transform.position.y, shadow.gameObject.transform.position.z));
-            if (shadow.gameObject.transform.position.x <= spawnXPosition + wiggleRoom * -1f)
-            {
-                hitRightBound = false;
-            }
-        }
-        if (!hitRightBound)
-        {
-            shadow.gameObject.rigidbody.MovePosition(new Vector3(shadow.gameObject.transform.position.x + shadowSeekSpeed * Time.deltaTime, shadow.gameObject.transform.position.y, shadow.gameObject.transform.position.z));
-            if (shadow.gameObject.transform.position.x >= spawnXPosition + wiggleRoom)
-            {
-                hitRightBound = true;
-            }
-        }
-        /*TODO: Move left to right.
-         * If Shoe in collider for X time, DropFoot();
-         */
-    }
-
-    //This will make the foot come straight down when the shoe (Player) is in the shadow for too long. 
     public void DropFoot ()
     {
-        gameObject.rigidbody.AddForce(0, - 900, 0);
-        //TODO: Foot moves straight down. 
+        foot.transform.parent = null;
+        foot.rigidbody.AddForce(0, - 900, 0);
+        Destroy(foot, 10);
+        
     }
 
-    /* This shit aint needed no more brah. 
-     * void OnTriggerEnter(Collider col)
-     {
-         Debug.Log(col.gameObject.name + "Collided!");
-     }
-
-     void OnTriggerStay(Collider col)
-     {
-         if (col.gameObject == shoe)
-         {
-             shadowTimer += Time.deltaTime;
-            // Debug.Log("Timer: " + shadowTimer);
-             if (shadowTimer >= stompTime)
-             {
-                 DropFoot();
-             }
-         }
-     }
-     void OnTriggerExit(Collider col)
-     {
-         if (col.gameObject == shoe)
-         {
-             shadowTimer = 0f;
-             Debug.Log("Timer Reset to 0f!");
-         }
-     }
-     */
 }
