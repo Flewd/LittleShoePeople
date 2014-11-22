ec4407fc1126feac04de052d21e76a850800edf4
Nick Mudry <nick@mudry.me>
2014-11-21 23:18:27 -0500

    Started Some Powerup Stuff

---
+++ Assets/Scripts/PlayerScoreScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScoreScript : MonoBehaviour {
+    public float score;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void AddScore (float amount)
+    {
+        score += amount;
+    }
+
+    public void SubtractScore (float amount)
+    {
+        score -= amount;
+    }
+}
